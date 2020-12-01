f406d89729b1d3e072ce23e4e7148d2990c1cc02
Elias Barbosa <69683964+EliasBarbosa214@users.noreply.github.com>
2020-12-01 17:16:32 -0300

    Add files via upload

---
+++ redGuy.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+
+class RedGuy(pygame.sprite.Sprite):
+
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.image = pygame.image.load('data/Imagens/RatoRato2.png')
+        self.image = pygame.transform.scale(self.image, [95, 60])
+        self.rect = pygame.Rect(60, 90, 60, 90)
+
+        self.rect.x = 200
+        self.rect.y = 300
+
+        self.rect.x = 940 + random.randint(1 , 400)
+        self.rect.y = random.randint(120, 550)
+        self.speed = 3 + random.random() * 2
+
+
+    def update(self, *args):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            self.kill()
